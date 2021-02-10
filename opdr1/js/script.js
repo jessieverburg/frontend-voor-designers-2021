e6b530ee8a86ca76bbdb8256ae3ff450fccb08d3
jessieverburg <74256343+jessieverburg@users.noreply.github.com>
2021-02-10 14:31:24 +0100

    Add files via upload

---
+++ opdr1/js/script.js
@@ -0,0 +1,13 @@
+var btn = document.getElementById('btn');
+
+function Toggle() {
+    if (btn.classList.contains("far")) {
+        btn.classList.remove("far");
+        btn.classList.add("fas");
+    }else{
+        btn.classList.remove("fas");
+        btn.classList.add("far");
+    }
+}
+
+btn.addEventListener('click', Toggle);
