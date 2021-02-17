225bae3ba62a8a29ce2c7b5b654ac7f3a49911a1
jessieverburg <74256343+jessieverburg@users.noreply.github.com>
2021-02-17 11:51:30 +0100

    Update script.js

--- opdracht1/v2/js/script.js
+++ opdracht1/v2/js/script.js
@@ -1,6 +1,5 @@
 var btn = document.getElementById('btn');
 
-
 function Toggle() {
     if (btn.classList.contains("far")) {
         btn.classList.remove("far");
@@ -11,4 +10,4 @@ function Toggle() {
     }
 }
 
-document.getElementByIdl('btn').forEach((btn) => btn.addEventListener('click', Toggle, (e) => { console.log('hello'); }))
+btn.addEventListener('click', Toggle);
